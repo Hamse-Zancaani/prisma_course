e37787d5d3ea690259624a04d1d1964caaeebd3b
Hamse Hassan <hamse>
2024-04-03 01:24:33 +0300

    first commit

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 import express from 'express';
+import { PrismaClient } from '@prisma/client';
 
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
+const Prisma = new PrismaClient();
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
     });
+
+app.post('/add-user', async(req, res) => {
+
+    const {name,age,email} = req.body;
+
+    const newUser= await Prisma.user.create({
+        data:{
+            name,
+            age,
+            email
+        }
+    })
+
+    res.status(200).json("User added successfully");
+
+
+});
 
 
 app.listen(port, () => {
