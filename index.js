475a872c23e13e9951adeb56bc6aa50f65385755
Hamse Hassan <hamse>
2024-04-05 02:04:14 +0300

    third  commit

--- index.js
+++ index.js
@@ -30,6 +30,47 @@ app.post('/add-user', async(req, res) => {
 });
 
 
+//get data and fetch it
+
+app.get('/get-user', async(req, res) => {
+    const users = await Prisma.user.findMany();
+    res.status(200).json(users);
+    });
+    
+    //update data
+
+    app.patch('/update-user/:id', async(req, res) => {
+        const id = parseInt(req.params.id);
+        const {name,age,email} = req.body;
+    
+        const updatedUser = await Prisma.user.update({
+            where:{
+                id
+            },
+            data:{
+                name,
+                age,
+                email
+            }
+        })
+    
+        res.status(200).json("User updated successfully");
+    });
+
+    //
+    app.delete('/delete-user/:id', async(req, res) => {
+        const id = parseInt(req.params.id);
+    
+        const deletedUser = await Prisma.user.delete({
+            where:{
+                id
+            }
+        })
+    
+        res.status(200).json("User deleted successfully");
+    });
+
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
     });
